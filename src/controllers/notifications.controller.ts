c3b082b4684ebabe5bd2bfc487fd4e852b48b6c3
zaib shamsi <135112519+zaibshamsi@users.noreply.github.com>
2025-07-14 18:18:20 +0530

    Update notifications.controller.ts

--- src/controllers/notifications.controller.ts
+++ src/controllers/notifications.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import {asyncHandler} from "../middlewares/asyncHandler.middleware";
 import NotificationModel from "../models/notifications.model";
 
+// This is to send the notifications to the members about deadlines and incomplete tasks
 export const getNotificationsController = asyncHandler(async (req: Request, res: Response) => {
   const userId = req.user?._id;
   const notifications = await NotificationModel.find({ userId }).sort({ createdAt: -1 });
